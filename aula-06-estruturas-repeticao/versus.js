5cdef8e1c5fc80dac57cb36c24369be48af3568f
kaykewillians <kaykewilliansv@gmail.com>
2026-06-01 16:25:31 -0300

    lista 4 e aula de hoje

---
+++ aula-06-estruturas-repeticao/versus.js
@@ -0,0 +1,7 @@
+// Rodar: npm install prompt-sync
+
+// Importa o modúlo (Para rodar algoritimo no terminal)
+const prompt = require('prompt-sync')()
+
+// Titulo 
+console.log ('---repetição de versos ---')
---
+++ aula-07-vetores-matrizes/exemplo.js
@@ -0,0 +1,9 @@
+// impotação da biblioteca pra rodar 
+const readline = require('prompt-sync')();
+
+console.log('--- Vetores com loop ---');
+console.log('Digite o nome de 5 filmes abaixo: ')
+
+
+filmes[i] = prompt (
+)
